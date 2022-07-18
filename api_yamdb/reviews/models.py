7fdc5d25e28fba42e3c7775729ca4527e779dc6a
Ilya Petrukhin <Tvules@mail.ru>
2022-07-18 22:21:18 +0400

    Создает модель Review.

---
+++ api_yamdb/reviews/models.py
@@ -0,0 +1,34 @@
+from django.contrib.auth import get_user_model
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+
+User = get_user_model()
+
+
+class Review(models.Model):
+    """Модель отзывов."""
+
+    title = models.ForeignKey(
+        'Title',
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='Произведение',
+    )
+    text = models.TextField('Текст')
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='Автор',
+    )
+    score = models.IntegerField(
+        'Оценка',
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(10)
+        ],
+    )
+    pub_date = models.DateTimeField(
+        'Дата публикации',
+        auto_now_add=True,
+    )
