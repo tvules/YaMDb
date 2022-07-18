6ae2b053b06be618ad4a89c554e576e268161322
Ilya Petrukhin <Tvules@mail.ru>
2022-07-18 22:50:01 +0400

    Создает модель Comment.

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -40,3 +40,33 @@ class Review(models.Model):
 
     def __str__(self):
         return self.text[:30]
+
+
+class Comment(models.Model):
+    """Модель комментария к отзыву."""
+
+    review = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Отзыв',
+    )
+    text = models.TextField('Текст')
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Автор',
+    )
+    pub_date = models.DateTimeField(
+        'Дата публикации',
+        auto_now_add=True,
+    )
+
+    class Meta:
+        ordering = ('-pub_date',)
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+
+    def __str__(self):
+        return self.text[:30]
