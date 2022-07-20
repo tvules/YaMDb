7cd11ad1855d591e2796463f16ca6f6c4834ef8e
Андрей Ростовцев <andronni4eg@yandex.ru>
2022-07-20 21:21:17 +0300

    зарегистрировал приложения drf и фильтры

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -21,6 +21,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'django_filters',
     'api.apps.ApiConfig',
     'reviews.apps.ReviewsConfig',
 ]
