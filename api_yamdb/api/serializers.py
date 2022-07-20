d2b9a95931aab15b451bd3c050a1826f3541286c
Ilya Petrukhin <Tvules@mail.ru>
2022-07-21 00:39:46 +0400

    Создает сериализатор модели "Review".

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,31 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import serializers
+
+from reviews.models import Title, Review
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    """Сериализатор модели Review."""
+
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True,
+    )
+
+    class Meta:
+        model = Review
+        fields = ('id', 'text', 'author', 'score', 'pub_date')
+
+    def validate(self, attrs):
+        author = self.context.get('request').user
+        title = get_object_or_404(
+            Title,
+            pk=self.context.get('view').kwargs.get('title_id'),
+        )
+
+        if title.reviews.filter(author=author).exists():
+            return serializers.ValidationError(
+                'Нельзя оставлять больше одного отзыва.'
+            )
+
+        return attrs
