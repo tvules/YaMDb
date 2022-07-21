a67a4c088da013f8f237c80aa128d19088ad5d13
Ilya Petrukhin <Tvules@mail.ru>
2022-07-21 17:37:39 +0400

    Refactoring: Исправление docstring, изменение метода у "IsStaffOrReadOnly".

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -2,7 +2,10 @@ from rest_framework import permissions
 
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
-    """Разрешение, позволяющее редактировать объект только владельцу."""
+    """
+    Разрешение на уровне объекта,
+    позволяющее редактировать его только владельцам объекта.
+    """
 
     def has_object_permission(self, request, view, obj):
         return (
@@ -12,9 +15,12 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
 
 
 class IsStaffOrReadOnly(permissions.BasePermission):
-    """Разрешение, позволяющее редактировать объект только персоналу."""
+    """
+    Разрешение на уровне объекта,
+    позволяющее редактировать его только персоналу.
+    """
 
-    def has_permission(self, request, view):
+    def has_object_permission(self, request, view, obj):
         return (
             request.method in permissions.SAFE_METHODS
             or request.user.is_staff
