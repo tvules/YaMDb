0640bcbabf57329606cfa6359f37a9cedd95eb5e
Максим Гребенюк <maxchad36@yandex.ru>
2022-07-23 08:17:33 +0300

    правки модели user

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -4,24 +4,27 @@ from django.db import models
 
 from .validators import validate_username
 
+ROLE_CHOICES = [
+    ("user", "user"),
+    ("moderator", "moderator"),
+    ("admin", "admin"),
+]
+
 
 class User(AbstractUser):
-    ROLE_CHOICES = [
-        ("user", "user"),
-        ("moderator", "moderator"),
-        ("admin", "admin"),
-    ]
     username = models.CharField(
         db_index=True,
         max_length=150,
         unique=True,
-        validators=[validate_username, UnicodeUsernameValidator]
+        validators=[UnicodeUsernameValidator, validate_username]
     )
     email = models.EmailField(unique=True, max_length=254)
     first_name = models.CharField(max_length=150, blank=True)
     last_name = models.CharField(max_length=150, blank=True)
-    bio = models.CharField(null=True, blank=True)
-    role = models.CharField(choices=ROLE_CHOICES, default="user")
+    bio = models.CharField(null=True, blank=True, max_length=150)
+    role = models.CharField(
+        choices=ROLE_CHOICES, default="user", max_length=15)
+    confirmation_code = models.CharField(editable=False, max_length=50)
 
     def __str__(self):
         return self.username
--- api_yamdb/users/validators.py
+++ api_yamdb/users/validators.py
@@ -1,11 +1,10 @@
 from django.core.exceptions import ValidationError
 
-forbidden_names = ['me', 'admin', 'moderator']
+forbidden_names = ('me', 'admin', 'moderator')
 
 
 def validate_username(value):
     if value in forbidden_names:
         raise ValidationError(
-            ('%(value)s выберете другое имя'),
-            params={'value': value},
+            ('Выберете другое имя'),
         )
