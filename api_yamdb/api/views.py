117c5e74acf21527e8fd00b9ef1bf7ba05ad1a24
Max-arys <95362757+Max-arys@users.noreply.github.com>
2022-07-25 21:26:58 +0300

    добавлен паджинатор, внесены правки view

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -3,14 +3,13 @@ import uuid
 from django.contrib.auth import get_user_model
 from django.core.mail import send_mail
 from django.shortcuts import get_object_or_404
-from rest_framework import mixins, status, viewsets
-from rest_framework.permissions import IsAdminUser
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import mixins, response, status, views, viewsets
 from rest_framework_simplejwt.tokens import RefreshToken
 
-from .permissions import UserMePermission
-from .serializers import GetTokenSerializer, UserMeSerializer, UserSerializer
+from .pagination import UserPagination
+from .permissions import IsAdminPermission, UserMePermission
+from .serializers import (GetTokenSerializer, SignupSerializer,
+                          UserMeSerializer, UserSerializer)
 
 User = get_user_model()
 
@@ -18,7 +17,8 @@ User = get_user_model()
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (IsAdminUser,)
+    permission_classes = (IsAdminPermission,)
+    pagination_class = UserPagination
     lookup_field = 'username'
 
 
@@ -32,32 +32,21 @@ class UserMeViewSet(mixins.RetrieveModelMixin,
         return get_object_or_404(User, username=self.request.user)
 
 
-class SignupView(APIView):
+class SignupView(views.APIView):
 
     def post(self, request):
         from_email = None
         confirmation_code = str(uuid.uuid4())
-        email = request.data.get('email')
-        if User.objects.filter(email=email).exists():
-            username = request.data.get('username')
-            send_mail(username, confirmation_code, from_email, [email])
-            user = get_object_or_404(User, email=email)
-            user.confirmation_code = confirmation_code
-            user.save()
-            return Response(
-                {'username': str(username),
-                 'email': str(email)},
-                status=status.HTTP_200_OK)
-        serializer = UserMeSerializer(data=request.data)
+        serializer = SignupSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         email = serializer.validated_data.get('email')
         username = serializer.validated_data.get('username')
         send_mail(username, confirmation_code, from_email, [email])
         serializer.save(confirmation_code=confirmation_code)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return response.Response(serializer.data, status=status.HTTP_200_OK)
 
 
-class GetTokenView(APIView):
+class GetTokenView(views.APIView):
 
     def post(self, request):
         serializer = GetTokenSerializer(data=request.data)
@@ -65,6 +54,6 @@ class GetTokenView(APIView):
         username = serializer.validated_data.get('username')
         user = User.objects.get(username=username)
         token = str(RefreshToken.for_user(user).access_token)
-        return Response(
+        return response.Response(
             {'token': str(token)},
             status=status.HTTP_200_OK)
