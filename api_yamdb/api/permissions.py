15e1d07c8b341928f2417105d6c38da1f1209c5d
Max-arys <95362757+Max-arys@users.noreply.github.com>
2022-07-28 10:27:31 +0300

    проверка Black

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -24,7 +24,8 @@ class IsAdminPermission(permissions.BasePermission):
     def has_permission(self, request, view):
         return (
             request.user.is_authenticated
-            and (request.user.is_admin or request.user.is_superuser))
+            and (request.user.is_admin or request.user.is_superuser)
+        )
 
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
