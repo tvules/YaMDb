24c81172bf683fae92b6ca837819e116edffb59a
Max-arys <95362757+Max-arys@users.noreply.github.com>
2022-07-29 01:22:23 +0300

    правки кода

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,66 +1,51 @@
 from rest_framework import permissions
 
 
-class IsModeratorOrReadOnly(permissions.BasePermission):
+class ReadOnly(permissions.BasePermission):
     """
-    Разрешение на уровне объекта,
-    позволяющее редактировать его только модераторам.
+    Разрешение, предоставляющее доступ
+    только если метод запроса является безопасным(чтение).
     """
-    message = 'Отказано в доступе!'
+
+    def has_permission(self, request, view):
+        return request.method in permissions.SAFE_METHODS
 
     def has_object_permission(self, request, view, obj):
-        return (
-            request.method in permissions.SAFE_METHODS
+        return request.method in permissions.SAFE_METHODS
+
+
+class IsAdmin(permissions.BasePermission):
+    """Разрешение, предоставляющее доступ только админу и суперюзеру."""
+
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and (
+            request.user.is_superuser or request.user.is_admin
+        )
+
+
+class IsStaff(permissions.BasePermission):
+    """
+    Разрешение на уровне объекта, предоставляющее доступ только персоналу.
+    """
+
+    def has_permission(self, request, view):
+        return request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        return request.user.is_authenticated and (
+            request.user.is_superuser
+            or request.user.is_admin
             or request.user.is_moderator
         )
 
 
-class IsAdminPermission(permissions.BasePermission):
-    """
-    Разрешение,позволяющее редактировать его только администраторам.
-    """
-    message = 'Отказано в доступе!'
-
-    def has_permission(self, request, view):
-        return (
-            request.user.is_authenticated
-            and (request.user.is_admin or request.user.is_superuser)
-        )
-
-
-class IsAuthorOrReadOnly(permissions.BasePermission):
+class IsAuthorOrStaff(IsStaff):
     """
     Разрешение на уровне объекта,
-    позволяющее редактировать его только владельцам объекта.
+    предоставляющее доступ только автору и персоналу.
     """
 
     def has_object_permission(self, request, view, obj):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or obj.author == request.user
-        )
-
-
-class IsStaffOrReadOnly(permissions.BasePermission):
-    """
-    Разрешение на уровне объекта,
-    позволяющее редактировать его только персоналу.
-    """
-
-    def has_object_permission(self, request, view, obj):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or request.user.is_staff
-        )
-
-
-class IsAdminOrReadOnly(permissions.BasePermission):
-
-    def has_permission(self, request, view):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or (
-                request.user.is_authenticated
-                and (request.user.role == 'admin' or request.user.is_superuser)
-            )
-        )
+        if obj.author == request.user:
+            return True
+        return super().has_object_permission(request, view, obj)
