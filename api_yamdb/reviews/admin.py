71c98957eea678655638b4805ce45f7bf7598b7e
Андрей Ростовцев <andronni4eg@yandex.ru>
2022-07-31 03:59:17 +0300

    админка для произведений, жанров и категорий

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,9 +1,25 @@
 from django.contrib import admin
 
-from .models import Comment, Review
+from .models import Category, Comment, Genre, Review, Title
 
 
 @admin.register(Review, Comment)
 class ReviewAdmin(admin.ModelAdmin):
     search_fields = ('title', 'text')
     list_filter = ('pub_date',)
+
+
+@admin.register(Title)
+class TitleAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+    list_filter = ('year', 'genre__name', 'category__name')
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
