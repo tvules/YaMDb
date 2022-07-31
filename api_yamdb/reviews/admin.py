46aab133f88bf7f9aa1f9fac96389d4fc962aba1
Max-arys <95362757+Max-arys@users.noreply.github.com>
2022-07-31 19:15:34 +0300

    добавлена модель User в админку

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 from .models import Comment, Review
+from users.models import User
 
 
-@admin.register(Review, Comment)
+@admin.register(Review, Comment, User)
 class ReviewAdmin(admin.ModelAdmin):
     search_fields = ('title', 'text')
     list_filter = ('pub_date',)
