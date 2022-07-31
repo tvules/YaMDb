a5443844ed2bf1b1aea5a25162965c89878d5d08
Max-arys <95362757+Max-arys@users.noreply.github.com>
2022-07-31 20:58:09 +0300

    добавлен User в админкуЭ
    Э

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -4,7 +4,12 @@ from .models import Comment, Review
 from users.models import User
 
 
-@admin.register(Review, Comment, User)
+@admin.register(Review, Comment)
 class ReviewAdmin(admin.ModelAdmin):
     search_fields = ('title', 'text')
     list_filter = ('pub_date',)
+
+
+@admin.register(User)
+class userAdmin(admin.ModelAdmin):
+    list_filter = ('username',)
