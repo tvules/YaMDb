1e54db4dd75e934b91ea7413b940c4a8511b0ad7
Max-arys <95362757+Max-arys@users.noreply.github.com>
2022-07-31 15:54:01 +0300

    добавлены константы выбора роли

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -4,14 +4,16 @@ from django.db import models
 
 from .validators import validate_username
 
-ROLE_CHOICES = [
-    ("user", "user"),
-    ("moderator", "moderator"),
-    ("admin", "admin"),
-]
-
 
 class User(AbstractUser):
+    ADMIN = 'admin'
+    MODERATOR = 'moderator'
+    USER = 'user'
+    ROLE_CHOICES = [
+        (ADMIN, 'admin'),
+        (MODERATOR, 'moderator'),
+        (USER, 'user'),
+    ]
     username = models.CharField(
         db_index=True,
         max_length=150,
@@ -22,20 +24,18 @@ class User(AbstractUser):
     first_name = models.CharField(max_length=150, blank=True)
     last_name = models.CharField(max_length=150, blank=True)
     bio = models.CharField(blank=True, max_length=150)
-    role = models.CharField(
-        choices=ROLE_CHOICES, default="user", max_length=15
-    )
+    role = models.CharField(choices=ROLE_CHOICES, default=USER, max_length=15)
     confirmation_code = models.CharField(
         editable=False, max_length=50, blank=True
     )
 
     @property
     def is_admin(self):
-        return self.role == 'admin'
+        return self.role == self.ADMIN
 
     @property
     def is_moderator(self):
-        return self.role == 'moderator'
+        return self.role == self.MODERATOR
 
     def __str__(self):
         return self.username
